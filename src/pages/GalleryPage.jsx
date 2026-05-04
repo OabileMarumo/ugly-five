a996bba8fb165089c140fb48535705590892b5a2
OabileMarumo <obmarumo48@gmail.com>
2026-05-04 10:18:22 +0200

    modified:   src/pages/GalleryPage.jsx

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,31 +1,5 @@
 import { useState } from "react";
-import IMG_0003 from "./Images/IMG-0003.JPG";
-import IMG_0017 from "./Images/IMG-0017.JPG";
-import IMG_0142 from "./Images/IMG-0142.JPG";
-import IMG_0145 from "./Images/IMG-0145.JPG";
-import IMG_0992 from "./Images/IMG-0992.JPG";
-import IMG_1014 from "./Images/IMG-1014.JPG";
-import IMG_1100 from "./Images/IMG-1100.JPG";
-import IMG_1199 from "./Images/IMG-1199.JPG";
-import IMG_1201 from "./Images/IMG-1201.JPG";
-import IMG_2038 from "./Images/IMG-2038.JPG";
-import IMG_2139 from "./Images/IMG-2139.JPG";
-import IMG_2498 from "./Images/IMG-2498.JPG";
-
-export const galleryImages = [
-  { id: "Impala", name: "Impala", src: IMG_0003, alt: "Impala grazing in the grasslands", caption: "Graceful Impala in the wild" },
-  { id: "Warthog", name: "Warthog", src: IMG_0017, alt: "Warthog in the wild", caption: "Playful Warthog in the savanna" },
-  { id: "Elephant bull", name: "Elephant", src: IMG_0142, alt: "Elephant bull", caption: "Majestic Elephant family" },
-  { id: "Swimming Elephants", name: "Swimming Elephants", src: IMG_0145, alt: "Swimming Elephants", caption: "Graceful Elephants in the water" },
-  { id: "Chobe Elephant", name: "Chobe Elephant", src: IMG_0992, alt: "Chobe Elephant", caption: "Gentle Giants of Chobe" },
-  { id: "Crossing the road", name: "crossing the road", src: IMG_1014, alt: "Chobe Elephant", caption: "Gentle Giants of Chobe" },
-  { id: "Lion Feasting", name: "Lion Feasting", src: IMG_1100, alt: "Lion Feasting", caption: "Powerful Lion enjoying a meal" },
-  { id: "baboon", name: "baboon", src: IMG_1199, alt: "baboon", caption: "Curious Baboon in the wild" },
-  { id: "close up of a baboon", name: "close up of a baboon", src: IMG_1201, alt: "close up of a baboon", caption: "Intimate look at a Baboon" },
-  { id: "lioness", name: "lioness", src: IMG_2038, alt: "lioness", caption: "Regal Lioness surveying her territory" },
-  { id: "Cheetah", name: "Cheetah", src: IMG_2139, alt: "Cheetah", caption: "Swift Cheetah on the prowl" },
-  { id: "Front facing elephant", name: "Front facing elephant", src: IMG_2498, alt: "Front facing elephant", caption: "Front facing elephant" },
-];
+import { galleryImages } from "../data/galleryImages";
 
 export default function GalleryPage() {
   const [selectedImage, setSelectedImage] = useState(null);
