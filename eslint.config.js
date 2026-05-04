86db5014367774798b3607b194d70a2284d58de9
OabileMarumo <obmarumo48@gmail.com>
2026-05-04 09:51:43 +0200

    modified:   eslint.config.js 	modified:   index.html 	renamed:    src/App.js -> src/App.jsx 	renamed:    src/components/AnimalCard.js -> src/components/AnimalCard.jsx 	renamed:    src/components/ContactForm.js -> src/components/ContactForm.jsx 	renamed:    src/components/Hero.js -> src/components/Hero.jsx 	renamed:    src/components/Navbar.js -> src/components/Navbar.jsx 	renamed:    src/components/TourCard.js -> src/components/TourCard.jsx 	new file:   src/data/galleryImages.js 	renamed:    src/main.js -> src/main.jsx 	renamed:    src/pages/ContactPage.js -> src/pages/ContactPage.jsx 	renamed:    src/pages/GalleryPage.js -> src/pages/GalleryPage.jsx

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,21 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['backend/**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    files: ['src/**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
---
+++ src/data/galleryImages.js
@@ -0,0 +1,13 @@
+import hyena from "./images/animals/hyena.jpg";
+import marabou from "./images/animals/marabou.jpg";
+import vulture from "./images/animals/vulture.jpg";
+import warthog from "./images/animals/warthog.jpg";
+import wildebeest from "./images/animals/wildebeest.jpg";
+
+export const galleryImages = [
+  { id: "Hyena", name: "Hyena", src: hyena, alt: "Hyena in the wild", caption: "The Hyena" },
+  { id: "Marabou", name: "Marabou", src: marabou, alt: "Marabou stork", caption: "The Marabou" },
+  { id: "Vulture", name: "Vulture", src: vulture, alt: "Vulture", caption: "The Vulture" },
+  { id: "Warthog", name: "Warthog", src: warthog, alt: "Warthog", caption: "The Warthog" },
+  { id: "Wildebeest", name: "Wildebeest", src: wildebeest, alt: "Wildebeest", caption: "The Wildebeest" },
+];
